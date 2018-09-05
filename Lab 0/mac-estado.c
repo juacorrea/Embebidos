b03aafbca5f6075aecfaedf2793e2787c30fa362
juacorrea <42742377+juacorrea@users.noreply.github.com>
2018-09-05 11:16:31 -0300

    Update mac-estado.c

--- Lab 0/mac-estado.c
+++ Lab 0/mac-estado.c
@@ -1,9 +1,9 @@
 #define LED_ROJO_Y_BUZZ			3 						//primer y segundo bit de SALIDA en 1 (buzz y led rojo encendido)
-#define CALDERA             	4 						//tercer bit de SALIDA en 1 (caldera encendida)
+#define CALDERA            	 	4 						//tercer bit de SALIDA en 1 (caldera encendida)
 #define LED_VERDE       		8						//cuarto bit de SALIDA en 1 (luz verde encendido)
-#define T_UMBRAL             	1 						//primer bit de ENTRADA en 1(temp por arriba umbral)
-#define BOT_APRETADO         	2 						//segundo bit ENTRADA en 1(boton apretado)
-#define T_OK					0						//Temperatura por debajo del umbral
+#define T_UMBRAL          	   	1 						//primer bit de ENTRADA en 1(temp por arriba umbral)
+#define BOT_APRETADO       	  	2 						//segundo bit ENTRADA en 1(boton apretado)
+#define T_OK				0						//Temperatura por debajo del umbral
 
 enum ESTADO
 {
@@ -32,7 +32,7 @@ i = 0;												//utilizada en estado Enfriando
 			case (Ok):
 				salida = LED_VERDE; 						// Led verde prendido y caldera apagada
 				
-				entrada = entrada & T_UMBRAL ;				//chequea si el primer bit fue seteado (temp por arriba del umbral), si es asi cambio de estado a No_Ok
+				entrada = entrada & T_UMBRAL ;					//chequea si el primer bit fue seteado (temp por arriba del umbral), si es asi cambio de estado a No_Ok
 				if (entrada == T_UMBRAL )    				
 					{
 						estado = No_Ok;
@@ -40,7 +40,7 @@ i = 0;												//utilizada en estado Enfriando
 			break;
 
 			case (No_Ok):
-				salida = LED_ROJO_Y_BUZZ;                	// Apago led verde, prendo rojo y buzzer
+				salida = LED_ROJO_Y_BUZZ;                		// Apago led verde, prendo rojo y buzzer
 				
 				entrada = entrada & BOT_APRETADO;			// Chequea si el segundo bit fue seteado (boton apretado), si es asi cambio de estado a Enfriando
 				if (entrada == BOT_APRETADO)            		
@@ -62,7 +62,7 @@ i = 0;												//utilizada en estado Enfriando
 	         else if ( i == 0)
 	         {
 	            i++;
-	            salida = CALDERA; 								//Enciende la caldera
+	            salida = CALDERA; 							//Enciende la caldera
 	         }
 			break;
 		}
